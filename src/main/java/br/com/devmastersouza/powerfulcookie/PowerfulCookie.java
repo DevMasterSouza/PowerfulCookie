3f3f30a0732418925033c63339836306806cdc75
Gabriel (MasterSouza) <devmastersouza@gmail.com>
2017-04-15 02:00:44 -0300

    start plugin

---
+++ src/main/java/br/com/devmastersouza/powerfulcookie/PowerfulCookie.java
@@ -0,0 +1,15 @@
+package br.com.devmastersouza.powerfulcookie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PowerfulCookie extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
