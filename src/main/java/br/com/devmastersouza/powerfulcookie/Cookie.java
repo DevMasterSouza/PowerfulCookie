217b3dc7c7b9675e1a09080324314a1c0f722bac
Gabriel (MasterSouza) <devmastersouza@gmail.com>
2017-04-15 02:24:59 -0300

    Cookie object

---
+++ src/main/java/br/com/devmastersouza/powerfulcookie/Cookie.java
@@ -0,0 +1,35 @@
+package br.com.devmastersouza.powerfulcookie;
+
+import org.bukkit.potion.PotionEffect;
+
+import java.util.List;
+
+/**
+ * Create by DevMasterSouza - email: devmastersouza@gmail.com
+ */
+public class Cookie {
+
+    private String name;
+    private List<PotionEffect> effects;
+
+    public Cookie(String name, List<PotionEffect> effects) {
+        this.name = name;
+        this.effects = effects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PotionEffect> getEffects() {
+        return effects;
+    }
+
+    public void setEffects(List<PotionEffect> effects) {
+        this.effects = effects;
+    }
+}
