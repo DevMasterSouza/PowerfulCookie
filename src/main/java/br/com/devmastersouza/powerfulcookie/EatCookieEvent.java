397d1963d6c2e0486395e41705302880b780f155
Gabriel (MasterSouza) <devmastersouza@gmail.com>
2017-04-15 17:28:26 -0300

    EatCookieEvent

---
+++ src/main/java/br/com/devmastersouza/powerfulcookie/EatCookieEvent.java
@@ -0,0 +1,53 @@
+package br.com.devmastersouza.powerfulcookie;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Project Others Projects.
+ * Create by DevMasterSouza - email: devmastersouza@gmail.com
+ */
+public class EatCookieEvent extends Event implements Cancellable{
+
+    private static HandlerList handlerList = new HandlerList();
+    private boolean cancelled = false;
+
+    private Player player;
+    private Cookie cookie;
+
+    public EatCookieEvent(Player player, Cookie cookie) {
+        this.player = player;
+        this.cookie = cookie;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Cookie getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(Cookie cookie) {
+        this.cookie = cookie;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList() {return handlerList;}
+}
