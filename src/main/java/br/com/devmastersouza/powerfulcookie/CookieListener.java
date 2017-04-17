e5ec9bd347e9c8aeebf3809b5005870b7955dcf1
Gabriel (MasterSouza) <devmastersouza@gmail.com>
2017-04-17 02:33:45 -0300

    applied Sound Radius

--- src/main/java/br/com/devmastersouza/powerfulcookie/CookieListener.java
+++ src/main/java/br/com/devmastersouza/powerfulcookie/CookieListener.java
@@ -46,6 +46,12 @@ public class CookieListener implements Listener {
                                 }
                                 if (cookie.getEatSound() != null) {
                                     event.getPlayer().playSound(event.getPlayer().getLocation(), cookie.getEatSound(), 1F, 1F);
+                                    if (cookie.getSoundRadius() > 0) {
+                                        for (Entity en : event.getPlayer().getNearbyEntities(cookie.getSoundRadius(), cookie.getSoundRadius(), cookie.getSoundRadius())) {
+                                            if (en instanceof Player)
+                                                ((Player) en).playSound(((Player) en).getLocation(), cookie.getEatSound(), 1F, 1F);
+                                        }
+                                    }
                                     if (cookie.isBroadcastSound()) {
                                         for (Player online : Util.getOnlinePlayers()) {
                                             online.playSound(online.getLocation(), cookie.getEatSound(), 1F, 1F);
